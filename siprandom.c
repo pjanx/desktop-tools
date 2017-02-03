dba39bb612ec7b61d0a440dfe0c3d048b5ecd2ba
Přemysl Janouch <p.janouch@gmail.com>
2017-02-03 22:58:45 +0100

    Fix and update LICENSE

--- siprandom.c
+++ siprandom.c
@@ -2,7 +2,6 @@
  * siprandom.c: relatively fast pseudo-random data generator
  *
  * Copyright (c) 2016, Přemysl Janouch <p.janouch@gmail.com>
- * All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
