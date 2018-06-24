2d66313208dd10d1f440a91f305e83e8d20ee80e
Přemysl Janouch <p@janouch.name>
2018-06-24 03:14:20 +0200

    Relicense to 0BSD, update mail address
    I've come to the conclusion that copyright mostly just stands in the way
    of software development.  In my jurisdiction I cannot give up my own
    copyright and 0BSD seems to be the closest thing to public domain.
    
    The updated mail address, also used in my author/committer lines,
    is shorter and looks nicer.  People rarely interact anyway.

--- priod.c
+++ priod.c
@@ -4,11 +4,10 @@
  * Thanks to http://netsplit.com/the-proc-connector-and-socket-filters
  * for showing the way around the proc connector and BPF.
  *
- * Copyright (c) 2017, Přemysl Janouch <p.janouch@gmail.com>
+ * Copyright (c) 2017, Přemysl Janouch <p@janouch.name>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * purpose with or without fee is hereby granted.
  *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
